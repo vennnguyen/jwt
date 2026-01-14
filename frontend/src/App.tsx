b724f391c5d401e73d4bf83e56c2b6db08497b1f
atuan41005 <tuan100205@gmail.com>
2026-01-14 20:37:58 +0700

    config react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import {BrowserRouter, Route, Routes} from "react-router"
+import SignInPage from "./pages/SignInPage"
+import SignUpPage from "./pages/SignUpPage"
+import Home from "./pages/Home"
+import {Toaster} from "sonner"
+
+function App() {
+  return <>
+  <Toaster richColors/>
+  <BrowserRouter>
+  {/* public routes */}
+  <Routes>
+      <Route path="/sign-in" element={<SignInPage/>}/>
+      <Route path="/sign-up" element={<SignUpPage/>}/>
+  </Routes>
+  {/* protected routes */}
+  <Route path="/" element={<Home/>}/>
+  </BrowserRouter>
+  </>
+}
+
+export default App
